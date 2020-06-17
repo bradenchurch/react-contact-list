d32eedce81baf0180e3baea2e8fe5ef6822308d4
Braden Church <bradenchurch@gmail.com>
2020-06-16 20:03:20 -0600

    added table/form for new contacts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
-
+import Contacts from './components/contacts/Contacts';
+import ContactForm from './components/contacts/ContactForm';
 class App extends Component {
   state = { contacts: [
-    { id: 1, firstName: 'John', phone: '123-123-1234'},
-    { id: 2, firstName: 'Jacob', phone: '124-124-1235'},
-    { id: 3, firstName: 'Jingle-Heimer', phone: '125-125-1236'},
+    { id: 1, firstName: 'John', phone: '123-123-1233'},
+    { id: 2, firstName: 'Sally', phone: '123-333-1233'},
+    { id: 3, firstName: 'Alex', phone: '123-125-1233'},
   ]}
-
-  render () {
+  getId = () => {
+    // NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+  addContact = (incomingContact) => {
+    const { contacts } = this.state
+    const newContact = { id: this.getId(), ...incomingContact }
+    this.setState({ contacts: [ newContact, ...contacts ]})
+  }
+  render() {
+    const { contacts } = this.state
     return(
       <div>
-        <Header size='huge' color='grey' textAlign='center'>Contact List</Header>
+        <ContactForm addContact={this.addContact} />
+        <Header size="huge" color='blue' textAlign='center'>
+          Contact list
+        </Header>
+        <Contacts contacts={contacts} />
       </div>
     )
   }
 }
-
-
 export default App;

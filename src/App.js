4c99e9d2fc2a92647de0f00faf2c394a63e6ff44
Braden Church <bradenchurch@gmail.com>
2020-06-16 19:02:02 -0600

    added styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { contacts: [
+    { id: 1, firstName: 'John', phone: '123-123-1234'},
+    { id: 2, firstName: 'Jacob', phone: '124-124-1235'},
+    { id: 3, firstName: 'Jingle-Heimer', phone: '125-125-1236'},
+  ]}
+
+  render () {
+    return(
+      <div>
+        <Header size='huge' color='grey' textAlign='center'>Contact List</Header>
+      </div>
+    )
+  }
 }
+
 
 export default App;
